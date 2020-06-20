2c749a11edfa37df2aea26ec481908f870c56930
Cameron Hudson <camnn3@yahoo.com>
2020-06-20 21:17:01 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,3 +1,5 @@
+# this can help a lot when we are coding something that you want to say a certain amount of times at one point but also multiple times in the code so instead of constantly typing it 8 times you just state its name give you the phrase 8 times 
+
 # Use this space to code along with the readme
 
 phrase = "Hello World!"
@@ -11,3 +13,16 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_wow_eight_times
+  phrase = "Wow!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+say_wow_eight_times
